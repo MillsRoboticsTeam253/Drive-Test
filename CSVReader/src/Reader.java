62ffbd5cad9a812a72a4f50095a2217a07211dce
Administrator <Administrator@TALAP20.mesd.k12.ca.us>
2018-01-20 15:29:29 -0800

    Added CSV reader

---
+++ CSVReader/src/Reader.java
@@ -0,0 +1,9 @@
+
+public class Reader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MotionProfileData data = new MotionProfileData("files/something_left.csv", false, false, false);
+	}
+
+}
